35159bdbd7250155fb8b2bf566830fdaa9fc7740
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2022-06-24 12:10:54 +0200

    extend test

--- pydrofoil/test/test_supportcoderiscv.py
+++ pydrofoil/test/test_supportcoderiscv.py
@@ -8,12 +8,15 @@ class TBM(supportcoderiscv.BlockMemory):
     BLOCK_MASK = BLOCK_SIZE - 1
     
 
-def test_mem():
+def test_mem_write_read():
     mem = TBM()
     addresses = range(100) + [mem.BLOCK_MASK + i for i in range(-100, 100)]
     for size in [1, 2, 4, 8]:
-        for addr in addresses:
-            data = r_uint(random.randrange(2**(size*8)))
-            mem.write(r_uint(addr), size, data)
-            assert mem.read(r_uint(addr), size) == data
+        for base_addr in addresses:
+            for consec in range(1, 20):
+                data = [r_uint(random.randrange(2**(size*8))) for _ in range(consec)]
+                for offset in range(consec):
+                    mem.write(r_uint(base_addr + offset * size), size, data[offset])
+                for offset in range(consec):
+                    assert mem.read(r_uint(base_addr + offset * size), size) == data[offset]
 

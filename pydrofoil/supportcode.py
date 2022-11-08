8701020b9870b05465e92ccceaced7ba9f028ac7
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2022-11-08 12:37:21 +0100

    reorder things

--- pydrofoil/supportcode.py
+++ pydrofoil/supportcode.py
@@ -96,8 +96,23 @@ make_dummy('zeros')
 
 # generic helpers
 
+def raise_type_error():
+    raise TypeError
+
+
+# bit vectors
+
 def zero_extend(machine, a, b):
     return a
+
+def fast_signed(machine, op, n):
+    if n == 64:
+        return intmask(op)
+    assert n > 0
+    u1 = r_uint(1)
+    m = u1 << (n - 1)
+    op = op & ((u1 << n) - 1) # mask off higher bits to be sure
+    return intmask((op ^ m) - m)
 
 @objectmodel.always_inline
 def add_bits_int(machine, a, b):
@@ -109,28 +124,9 @@ def sub_bits_int(machine, a, b):
 def length(machine, gbv):
     return gbv.size_as_int()
 
-def fast_signed(machine, op, n):
-    if n == 64:
-        return intmask(op)
-    assert n > 0
-    u1 = r_uint(1)
-    m = u1 << (n - 1)
-    op = op & ((u1 << n) - 1) # mask off higher bits to be sure
-    return intmask((op ^ m) - m)
-
 def sign_extend(machine, gbv, lint):
     size = lint.toint()
     return gbv.sign_extend(size)
-
-def raise_type_error():
-    raise TypeError
-
-def eq_int(machine, a, b):
-    assert isinstance(a, Integer)
-    return a.eq(b)
-
-def eq_bit(machine, a, b):
-    return a == b
 
 def eq_bits(machine, gvba, gvbb):
     return gvba.eq(gvbb)
@@ -147,11 +143,65 @@ def or_bits(machine, gvba, gvbb):
 def not_bits(machine, gvba):
     return gvba.invert()
 
-def safe_rshift(machine, n, shift):
-    assert shift >= 0
-    if shift >= 64:
-        return r_uint(0)
-    return n >> shift
+def print_bits(machine, s, b):
+    print s + b.string_of_bits()
+    return ()
+
+def shiftl(machine, gbv, i):
+    return gbv.lshift(i.toint())
+
+def shiftr(machine, gbv, i):
+    return gbv.rshift(i.toint())
+
+def shift_bits_left(machine, gbv, gbva):
+    return gbv.lshift_bits(gbva)
+
+def shift_bits_right(machine, gbv, gbva):
+    return gbv.rshift_bits(gbva)
+
+def sail_unsigned(machine, gbv):
+    return gbv.unsigned()
+
+def sail_signed(machine, gbv):
+    return gbv.signed()
+
+def append_64(machine, bv, v):
+    return bv.append_64(v)
+
+def vector_update(machine, bv, index, element):
+    return bv.update_bit(index.toint(), element)
+
+def vector_access(machine, bv, index):
+    return bv.read_bit(index.toint())
+
+def update_fbits(fb, index, element):
+    assert 0 <= index < 64
+    if element:
+        return fb | (r_uint(1) << index)
+    else:
+        return fb & ~(r_uint(1) << index)
+
+def vector_update_subrange(machine, bv, n, m, s):
+    return bv.update_subrange(n.toint(), m.toint(), s)
+
+def vector_subrange(machine, bv, n, m):
+    return bv.subrange(n.toint(), m.toint())
+
+def string_of_bits(machine, gbv):
+    return gbv.string_of_bits()
+
+def decimal_string_of_bits(machine, sbits):
+    return str(sbits)
+
+
+# integers
+
+def eq_int(machine, a, b):
+    assert isinstance(a, Integer)
+    return a.eq(b)
+
+def eq_bit(machine, a, b):
+    return a == b
 
 def lteq(machine, ia, ib):
     return ia.le(ib)
@@ -198,53 +248,43 @@ def min_int(machine, ia, ib):
         return ia
     return ib
 
-def print_endline(machine, s):
-    print s
-    return ()
+def get_slice_int(machine, len, n, start):
+    return n.slice(len.toint(), start.toint())
 
-def print_bits(machine, s, b):
-    print s + b.string_of_bits()
-    return ()
+def safe_rshift(machine, n, shift):
+    assert shift >= 0
+    if shift >= 64:
+        return r_uint(0)
+    return n >> shift
 
 def print_int(machine, s, i):
     print s + i.str()
     return ()
 
+def not_(machine, b):
+    return not b
+
+def eq_bool(machine, a, b):
+    return a == b
+
+def string_of_int(machine, r):
+    return r.str()
+
+
+# various
+
 @objectmodel.specialize.argtype(1)
 def reg_deref(machine, s):
     return s
-
-def not_(machine, b):
-    return not b
 
 def sail_assert(cond, st):
     if not objectmodel.we_are_translated() and not cond:
         import pdb; pdb.set_trace()
     assert cond, st
 
-def eq_bool(machine, a, b):
-    return a == b
-
-def shiftl(machine, gbv, i):
-    return gbv.lshift(i.toint())
-
-def shiftr(machine, gbv, i):
-    return gbv.rshift(i.toint())
-
-def shift_bits_left(machine, gbv, gbva):
-    return gbv.lshift_bits(gbva)
-
-def shift_bits_right(machine, gbv, gbva):
-    return gbv.rshift_bits(gbva)
-
-def sail_unsigned(machine, gbv):
-    return gbv.unsigned()
-
-def sail_signed(machine, gbv):
-    return gbv.signed()
-
-def append_64(machine, bv, v):
-    return bv.append_64(v)
+def print_endline(machine, s):
+    print s
+    return ()
 
 # vector stuff
 
@@ -256,31 +296,8 @@ def vector_update_inplace(machine, res, l, index, element):
     l[index] = element
     return l
 
-def vector_update(machine, bv, index, element):
-    return bv.update_bit(index.toint(), element)
-
-def vector_access(machine, bv, index):
-    return bv.read_bit(index.toint())
-
-def update_fbits(fb, index, element):
-    assert 0 <= index < 64
-    if element:
-        return fb | (r_uint(1) << index)
-    else:
-        return fb & ~(r_uint(1) << index)
-
-def vector_update_subrange(machine, bv, n, m, s):
-    return bv.update_subrange(n.toint(), m.toint(), s)
-
-def vector_subrange(machine, bv, n, m):
-    return bv.subrange(n.toint(), m.toint())
-
-
 def elf_tohost(machine, _):
     return Integer.fromint(0)
-
-def get_slice_int(machine, len, n, start):
-    return n.slice(len.toint(), start.toint())
 
 def platform_barrier(machine, _):
     return ()
@@ -293,16 +310,6 @@ def platform_write_mem_ea(machine, write_kind, addr_size, addr, n):
 
 def concat_str(machine, a, b):
     return a + b
-
-
-def string_of_bits(machine, gbv):
-    return gbv.string_of_bits()
-
-def decimal_string_of_bits(machine, sbits):
-    return str(sbits)
-    
-def string_of_int(machine, r):
-    return r.str()
 
 
 # argument handling

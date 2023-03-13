463d1b8f73258aeab6f7fc45931f6e1818ff57b8
Tim Matussek <timat102@hhu.de>
2023-03-13 16:19:23 +0100

    parse a subset of gdb remote protocol packets

--- riscv/plugin/gdb_pydrofoil/__init__.py
+++ riscv/plugin/gdb_pydrofoil/__init__.py
@@ -2,21 +2,96 @@ from dataclasses import dataclass
 
 @dataclass
 class GDBPacket:
-    command: bytes
+    command: str
+    args: bytes
 
-def _calculate_checksum(command: bytes):
+def _calculate_checksum(command: bytes) -> int:
     sum = 0
     for b in command:
         sum += b
     return sum % 256
 
-def _parse_gdb_packet(package: bytes):
-    if not package.startswith(b"$"):
+def _parse_command(packet: bytes) -> str:
+    if packet.startswith((b"v", b"q")):
+        cmd = packet.split(b":", 1)[0]
+    else:
+        cmd = packet[:1]
+    return str(cmd, "ascii")
+
+def _parse_gdb_packet(packet: bytes) -> GDBPacket:
+    if packet.startswith(b"+") or packet.startswith(b"-"):
+        # ignore acknowledgments
+        # TODO "-" should retransmit previous command
+        packet = packet[1:]
+
+    if not packet.startswith(b"$"):
         raise ValueError()
 
-    command, checksum = package[1:].split(b"#", 1)
+    cmd_and_args, checksum = packet[1:].split(b"#", 1)
     checksum = int(checksum, 16)
-    if _calculate_checksum(command) != checksum:
+    if _calculate_checksum(cmd_and_args) != checksum:
         raise ValueError()
 
-    return GDBPacket(command)
+    cmd = _parse_command(cmd_and_args)
+    args = cmd_and_args[len(cmd):]
+
+    return GDBPacket(cmd, args)
+
+def _make_packet(cmd_and_args: bytes) -> bytes:
+    checksum = format(_calculate_checksum(cmd_and_args), "02x")
+    return b"+$" + cmd_and_args + b"#" + checksum.encode("ascii")
+    
+class GDBServer:
+    def __init__(self, machine):
+        self.machine = machine
+        
+    def handle(self, packet: bytes) -> bytes:
+        packet = _parse_gdb_packet(packet)
+        method = getattr(self, packet.command, None)
+        if method is not None:
+            return method(packet)
+        else:
+            return _make_packet(b"") # empty response signals not supported
+
+    def m(self, packet: GDBPacket) -> bytes:
+        addr, length = packet.args.split(b",", 1)
+        addr = int(addr, 16)
+        length = int(length, 16)
+
+        memory = ""
+        for i in range(length):
+            memory += format(self.machine.read_memory(addr + i), "02x")
+
+        return _make_packet(memory.encode("ascii"))
+
+    def M(self, packet: GDBPacket) -> bytes:
+        args, data = packet.args.split(b":", 1)
+        addr, length = args.split(b",", 1)
+        addr = int(addr, 16)
+        length = int(length, 16)
+
+        for i in range(length):
+            value = int(data[i*2:i*2+2], 16)
+            self.machine.write_memory(addr + i, value)
+
+        return _make_packet(b"OK")
+
+    def s(self, packet: GDBPacket):
+        if len(packet.args) != 0:
+            addr = int(packet.args, 16)
+            self.machine.write_register("pc", addr)
+
+        self.machine.step()
+
+        return _make_packet(b"S05") # TODO is this the correct reply?
+
+    def X(self, packet: GDBPacket) -> bytes:
+        args, data = packet.args.split(b":", 1)
+        addr, length = args.split(b",", 1)
+        addr = int(addr, 16)
+        length = int(length, 16)
+
+        for i in range(length):
+            self.machine.write_memory(addr + i, data[i])
+
+        return _make_packet(b"OK")
--- riscv/plugin/gdb_pydrofoil/test/test_gdb_pydrofoil.py
+++ riscv/plugin/gdb_pydrofoil/test/test_gdb_pydrofoil.py
@@ -1,12 +1,126 @@
 import gdb_pydrofoil
+from gdb_pydrofoil import GDBPacket, GDBServer
 import pytest
 
 def test_parse_gdb_packet():
     packet = b"$vMustReplyEmpty#3a"
     res = gdb_pydrofoil._parse_gdb_packet(packet)
-    assert res.command == b"vMustReplyEmpty"
+    assert res.command == "vMustReplyEmpty"
 
 def test_parse_gdb_packet_checksum_wrong():
     packet = b"$vMustReplyEmpty#00"
     with pytest.raises(ValueError):
         gdb_pydrofoil._parse_gdb_packet(packet)
+
+def test_parse_gdb_packet_ack():
+    packet = b"+$vMustReplyEmpty#3a"
+    res = gdb_pydrofoil._parse_gdb_packet(packet)
+    assert res.command == "vMustReplyEmpty"
+
+def test_parse_gdb_packet_ack2():
+    packet = b"-$vMustReplyEmpty#3a"
+    res = gdb_pydrofoil._parse_gdb_packet(packet)
+    assert res.command == "vMustReplyEmpty"
+        
+def test_get_command_s():
+    command = gdb_pydrofoil._parse_command(b"s")
+    assert command == "s"
+    
+def test_get_command_qSupported():
+    command = gdb_pydrofoil._parse_command(b"qSupported")
+    assert command == "qSupported"
+    
+def test_get_command_s_attributes():
+    command = gdb_pydrofoil._parse_command(b"sab12")
+    assert command == "s"
+
+def test_handle_unsupported():
+    machine = "dummy"
+    server = GDBServer(machine)
+    resp = server.handle(b"$_#5f")
+    assert resp == b"+$#00"
+
+def test_handle_m():
+    class DummyMachine:
+        def __init__(self):
+            self.addrs = []
+
+        def read_memory(self, addr):
+            self.addrs.append(addr)
+            return 1
+
+    machine = DummyMachine()
+    server = GDBServer(machine)
+    resp = server.handle(b"$m10,5#2f")
+    assert resp == b"+$0101010101#e5"
+    assert machine.addrs == [16, 17, 18, 19, 20]
+
+def test_handle_M():
+    class DummyMachine:
+        def __init__(self):
+            self.addrs = []
+            self.values = []
+
+        def write_memory(self, addr, value):
+            self.addrs.append(addr)
+            self.values.append(value)
+
+    machine = DummyMachine()
+    server = GDBServer(machine)
+    resp = server.handle(b"$M10,5:0102030405#38")
+    assert resp == b"+$OK#9a"
+    assert machine.addrs == [16, 17, 18, 19, 20]
+    assert machine.values == [1, 2, 3, 4, 5]
+
+def test_handle_s():
+    class DummyMachine:
+        def __init__(self):
+            self.has_been_called = False
+
+        def step(self):
+            self.has_been_called = True
+
+    machine = DummyMachine()
+    server = GDBServer(machine)
+    resp = server.handle(b"$s#73")
+    assert resp == b"+$S05#b8"
+    assert machine.has_been_called
+
+def test_handle_s_addr():
+    class DummyMachine:
+        def __init__(self):
+            self.register = ""
+            self.addr = 0
+            self.has_been_called = False
+
+        def write_register(self, r, v):
+            self.register = r
+            self.addr = v
+
+        def step(self):
+            self.has_been_called = True
+
+    machine = DummyMachine()
+    server = GDBServer(machine)
+    resp = server.handle(b"$s10#d4")
+    assert resp == b"+$S05#b8"
+    assert machine.has_been_called
+    assert machine.register == "pc"
+    assert machine.addr == 16
+
+def test_handle_X():
+    class DummyMachine:
+        def __init__(self):
+            self.addrs = []
+            self.values = []
+
+        def write_memory(self, addr, value):
+            self.addrs.append(addr)
+            self.values.append(value)
+
+    machine = DummyMachine()
+    server = GDBServer(machine)
+    resp = server.handle(b"$X10,5:\1\2\3\4\5#63")
+    assert resp == b"+$OK#9a"
+    assert machine.addrs == [16, 17, 18, 19, 20]
+    assert machine.values == [1, 2, 3, 4, 5]

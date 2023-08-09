6704f23c2dce23a1d0e852a1518924b66c09d139
Jubo Xu <jx1820@ic.ac.uk>
2023-08-09 13:33:24 +0100

    commit with removing comments now

--- pydrofoil/real.py
+++ pydrofoil/real.py
@@ -15,35 +15,6 @@ class Real(object):
     @staticmethod
     def fromint(num, den=1):
         assert not den==0, "denominator cannot be 0"
-        # if ((num <= -2**63 or num >= 2**63) and den > 0) or (den >= 2**63):
-        #     num = rbigint.fromlong(num)
-        #     den = rbigint.fromlong(den)
-        # elif ((num <= -2**63 or num >= 2**63) and den <0) or (den <= -2**63):
-        #     num = rbigint.fromlong(-num)
-        #     den = rbigint.fromlong(-den)
-        # else:
-        # if ((num == MININT or num == MAXINT) and (den > 0 and den <= MAXINT)) or ((den == MAXINT) and ((num > 0 and num <= MAXINT) or (num < 0 and num >= MININT))):
-        #     num = rbigint.fromint(num)
-        #     den = rbigint.fromint(den)
-        # elif ((num == MININT or num == MAXINT) and (den < 0 and den >= MININT)) or ((den == MININT) and ((num > 0 and num <= MAXINT) or (num < 0 and num >= MININT))):
-        #     if (num == MININT and den == MININT):
-        #         num = rbigint.fromint(1)
-        #         den = rbigint.fromint(1)
-        #     elif num == MININT:
-        #         num = rbigint.fromint(MAXINT).add(rbigint.fromint(1))
-        #         den = rbigint.fromint(-den)
-        #     elif den == MININT:
-        #         num = rbigint.fromint(-num)
-        #         den = rbigint.fromint(MAXINT).add(rbigint.fromint(1))
-        #     else:                
-        #         num = rbigint.fromint(-num)
-        #         den = rbigint.fromint(-den)
-        # elif ((num > MININT and num < MAXINT) and (den > MININT and den < MAXINT)):
-        #     sign = num/abs(num)*den/abs(den)
-        #     num = rbigint.fromint(abs(num)*sign)
-        #     den = rbigint.fromint(abs(den))
-        # else:
-        #     assert False, "input is out of range of INT"
         if num == MININT or den == MININT:
             num = rbigint.fromint(num)
             den = rbigint.fromint(den)
@@ -56,103 +27,8 @@ class Real(object):
             sign = num/abs(num)*den/abs(den)
             num = rbigint.fromint(abs(num)*sign)
             den = rbigint.fromint(abs(den))
-        # assert not den.int_eq(0), "denominator cannot be 0"
         return Real(num, den)
     
-    # @staticmethod
-    # def fromstr(str):
-    #     from rpython.rlib.rstring import strip_spaces
-    #     s = strip_spaces(str)
-    #     j = -1
-    #     lead_zero_count = 0
-    #     ZERO_COUNT_END = False
-    #     for i in range(0, len(s)):
-    #         if s[i].isdigit() and s[i] != "0":
-    #             ZERO_COUNT_END = True
-    #         if (not ZERO_COUNT_END) and s[i] == "0":
-    #             lead_zero_count += 1 
-    #         if not s[i].isdigit() and s[i] != "+" and s[i] != "-":
-    #             if i == 1 or (i == 2 and (s[0] == "+" or s[0] == "-")):
-    #                 for j in range(len(s)-1, i, -1):
-    #                     if (s[j] == "+" or s[j] == "-") and s[j-1] == "e":
-    #                         break
-    #             for k in range(i+1, len(s)):
-    #                 if (not ZERO_COUNT_END) and s[k] == "0":
-    #                     lead_zero_count += 1
-    #                 else:
-    #                     break
-    #             break
-    #     # return i, s[:i]+s[i+1:]
-    #     if j == -1 or (j == 2 and (s[0] != "+" and s[0] != "-")) or (j == 3 and (s[0] == "+" or s[0] == "-")):
-    #         if lead_zero_count == len(s)-1:
-    #             return Real(rbigint.fromint(0), rbigint.fromint(1))
-    #         num = rbigint.fromstr((s[:i] + s[i+1:])[lead_zero_count:] if (s[0] != "+" and s[0] != "-") else s[0]+(s[1:i] + s[i+1:])[lead_zero_count:]) if i < len(s)-1 else rbigint.fromstr(s)
-    #         dif = len(s)-1-i
-    #         if dif < 19:
-    #             den = rbigint.fromint(10**dif)
-    #         else:
-    #             den = den_of_fromstr(dif)
-    #         return Real(num, den)
-    #     else:
-    #         if s[j] == "+":
-    #             shift = int(s[j+1:])
-    #             if shift >= j-i-2:
-    #                 num = rbigint.fromstr(s[:i]+s[i+1:j-1]+"0"*(shift-(j-i-2)))
-    #                 den = rbigint.fromint(1)
-    #                 return Real(num, den)
-    #             else:
-    #                 return Real.fromstr(s[:i]+s[i+1:i+1+shift]+s[i]+s[i+1+shift:j-1])
-    #             # 1.222e+5
-    #             # 1.222222e+3
-    #         elif s[j] == "-":
-    #             shift = int(s[j+1:])
-    #             num = rbigint.fromstr(s[:i]+s[i+1:j-1])
-    #             dif = j-i-2+shift
-    #             if dif < 19:
-    #                 den = rbigint.fromint(10**dif)
-    #             else:
-    #                 den = den_of_fromstr(dif)
-    #             return Real(num, den)
-
-    # string to real without considering about input as x.xxxxe+xxx or x.xxxxe-xxxx
-    # @staticmethod
-    # def fromstr(str):
-    #     from rpython.rlib.rstring import strip_spaces
-    #     s = strip_spaces(str)
-    #     lead_zero_count = 0
-    #     ZERO_COUNT_END = False
-    #     for i in range(0, len(s)):
-    #         if s[i].isdigit() and s[i] != "0":
-    #             ZERO_COUNT_END = True
-    #         if (not ZERO_COUNT_END) and s[i] == "0":
-    #             lead_zero_count += 1 
-    #         if not s[i].isdigit() and s[i] != "+" and s[i] != "-":
-    #             for j in range(i+1, len(s)):
-    #                 if (not ZERO_COUNT_END) and s[j] == "0":
-    #                     lead_zero_count += 1
-    #                 else:
-    #                     break
-    #             break
-    #     # return i, s[:i]+s[i+1:]
-    #     if lead_zero_count == len(s)-1:
-    #         return Real(rbigint.fromint(0), rbigint.fromint(1))
-    #     num = rbigint.fromstr((s[:i] + s[i+1:])[lead_zero_count:] if (s[0] != "+" and s[0] != "-") else s[0]+(s[1:i] + s[i+1:])[lead_zero_count:]) if i < len(s)-1 else rbigint.fromstr(s)
-    #     dif = len(s)-1-i
-    #     if dif < 19:
-    #         den = rbigint.fromint(10**dif)
-    #     else:
-    #         den = den_of_fromstr(dif)
-    #     return Real(num, den)
-    #     # num = ((s[:i] + s[i+1:])[lead_zero_count:] if (s[0] != "+" and s[0] != "-") else s[0]+(s[1:i] + s[i+1:])[lead_zero_count:]) if i < len(s)-1 else (s)
-    #     # dif = len(s)-1-i
-    #     # if dif < 19:
-    #     #     den = rbigint.fromint(10**dif)
-    #     # else:
-    #     #     den = den_of_fromstr(dif)
-    #     # # return Real(num, den)
-    #     # return lead_zero_count, num, den.str()
-
-    # Simplest implementation of fromstr with input format only as xxx.xxxx, leading zero issue resolved by setting base = 10 for rbigint.fromstr()
     @staticmethod
     def fromstr(str):
         from rpython.rlib.rstring import strip_spaces
@@ -196,37 +72,6 @@ class Real(object):
             den_new = den_new.neg()
         return Real(num_new, den_new)
 
-    # def pow(self, n):
-    #     if isinstance(n, int):
-    #         if n == 0:
-    #             return Real(rbigint.fromint(1), rbigint.fromint(1))
-    #         elif n < MININT or n > MAXINT:
-    #             assert False, "exponent is out of range of INT"
-    #         else:
-    #             n = rbigint.fromint(n)
-    #             num_new = self.num.pow(n)
-    #             den_new = self.den.pow(n)
-    #             return Real(num_new, den_new)
-    #     elif isinstance(n, rbigint):
-    #         if n.int_eq(0):
-    #             return Real(rbigint.fromint(1), rbigint.fromint(1))
-    #         else:
-    #             n = rbigint.fromint(n)
-    #             num_new = self.num.pow(n)
-    #             den_new = self.den.pow(n)
-    #             return Real(num_new, den_new)
-    #     elif isinstance(n, Real):
-    #         if n.num.int_eq(0):
-    #             return Real(rbigint.fromint(1), rbigint.fromint(1))
-    #         else:
-    #             if n.den.int_eq(1):
-    #                 num_new = self.num.pow(n.num)
-    #                 den_new = self.den.pow(n.num)
-    #                 return Real(num_new, den_new)
-    #             else:
-    #                 assert False, "exponent cannot be fraction"
-    #     else:
-    #         raise TypeError("exponent doesn't support this type")
     def pow(self, n):
         if n == 0:
             return Real(rbigint.fromint(1), rbigint.fromint(1))
@@ -236,10 +81,7 @@ class Real(object):
             n = rbigint.fromint(n)
             num_new = self.num.pow(n)
             den_new = self.den.pow(n)
-            return Real(num_new, den_new)
-        
-        
-            
+            return Real(num_new, den_new)       
     
     def neg(self):
         return Real(self.num.neg(), self.den)
@@ -247,15 +89,6 @@ class Real(object):
     def abs(self):
         return Real(self.num.abs(), self.den)
 
-    # def ceil(self):
-    #     diff = self.num.divmod(self.den)
-    #     num_new = self.num if diff[1].int_eq(0) else self.num.add(self.den.sub(diff[1]))
-    #     return Real(num_new, self.den)
-
-    # def floor(self):
-    #     diff = self.num.divmod(self.den)
-    #     num_new = self.num if diff[1].int_eq(0) else self.num.sub(diff[1])
-    #     return Real(num_new, self.den)
     def ceil(self):
         return self.num if self.den.int_eq(1) else self.num.floordiv(self.den).int_add(1)
     
@@ -283,13 +116,3 @@ class Real(object):
     
     def totuple(self):
         return self.num.toint(), self.den.toint()
-    
-
-# Helper function for fromstr()
-def den_of_fromstr(dif):
-    if dif < 19:
-        quo = dif // 2
-        rem = dif % 2
-        return rbigint.fromint(10**(2*quo)*10**(rem))
-    else:
-        return rbigint.fromint(10**18).mul(den_of_fromstr(dif-18))

a9b3e29933218fc3607da62204238fa962584af6
Jubo Xu <jx1820@ic.ac.uk>
2023-08-11 14:31:56 +0100

    commit with changing string += string to join of list

--- pydrofoil/mangle.py
+++ pydrofoil/mangle.py
@@ -1,51 +1,50 @@
-import string
 
 def mangle(str_in):
-    str_out = "z"
-    for i in range(0, len(str_in)):
-        c = ord(str_in[i])
+    out = ["z"]
+    for char in str_in:
+        c = ord(char)
         if c <= 41:
-            str_out += "z"
-            str_out += chr(c+16)
+            out.append("z")
+            out.append(chr(c+16))
         elif c <= 47:
-            str_out += "z"
-            str_out += chr(c+23)
-        elif c > 57 and c <= 64:
-            str_out += "z"
-            str_out += chr(c+13)
-        elif (c > 90 and c <= 94) or c == 96:
-            str_out += "z"
-            str_out += chr(c-13)
+            out.append("z")
+            out.append(chr(c+23))
+        elif 57 < c <= 64:
+            out.append("z")
+            out.append(chr(c+13))
+        elif 90 < c <= 94 or c == 96:
+            out.append("z")
+            out.append(chr(c-13))
         elif c == 0x7a:
-            str_out += "zz"
-        elif c > 122 and c <= 126:
-            str_out += "z"
-            str_out += chr(c-39)
+            out.append("zz")
+        elif 122 < c <= 126:
+            out.append("z")
+            out.append(chr(c-39))
         else:
-            str_out += str_in[i]
-    return str_out
+            out.append(char)
+    return "".join(out)
 
 def demangle(str_in):
-    str_out = ""
+    out = []
     next_encoded = False
     for i in range(1, len(str_in)):
         c = ord(str_in[i])
         if next_encoded:
             if c <= 57:
-                str_out += chr(c-16)
+                out.append(chr(c-16))
             elif c <= 70:
-                str_out += chr(c-23)
+                out.append(chr(c-23))
             elif c <= 77:
-                str_out += chr(c-13)
+                out.append(chr(c-13))
             elif c <= 83:
-                str_out += chr(c+13)
+                out.append(chr(c+13))
             elif c == 122:
-                str_out += chr(122)
+                out.append("z")
             else:
-                str_out += chr(c+39)
+                out.append(chr(c+39))
             next_encoded = False
         elif c == 0x7a:
             next_encoded = True
         else:
-            str_out += str_in[i]
-    return str_out
+            out.append(str_in[i])
+    return "".join(out)

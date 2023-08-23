19a975e02f941b8e5e7fcd54d77457d52f303fc1
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2023-08-23 10:09:35 +0200

    add hacky commandline argument to skip regeneration of ARM python code

--- arm/targetarm.py
+++ arm/targetarm.py
@@ -5,26 +5,29 @@ toplevel = dirname(dirname(__file__))
 armir = os.path.join(toplevel, "arm", "armv9.ir")
 outarm = os.path.join(toplevel, "arm", "generated", "outarm.py")
 
-def make_code():
+def make_code(regen=True):
     from arm import supportcodearm
-    outarm = _make_code()
+    outarm = _make_code(regen)
     return supportcodearm.get_main(outarm)
 
-def _make_code():
+def _make_code(regen=True):
     print "making python code"
-    #with open(armir, "rb") as f:
-    #    s = f.read()
-    #support_code = "from arm import supportcodearm as supportcode"
-    #res = parse_and_make_code(s, support_code)
-    #with open(outarm, "w") as f:
-    #    f.write(res)
-    #print "written file", outarm, "importing now"
+    if regen:
+        with open(armir, "rb") as f:
+            s = f.read()
+        support_code = "from arm import supportcodearm as supportcode"
+        res = parse_and_make_code(s, support_code)
+        with open(outarm, "w") as f:
+            f.write(res)
+        print "written file", outarm, "importing now"
+    else:
+        print "skipping regeneration, importing"
     from arm.generated import outarm as mod
     print "done"
     return mod
 
-def target(*args):
-    main = make_code()
+def target(driver, cmdlineargs):
+    main = make_code(regen="--no-arm-regen" not in cmdlineargs)
     print "translating to C!"
     return main
 

fb12971f5c0b00e2e6f9886546685ddc6f251ba2
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2023-09-02 10:57:41 +0200

    new jit idea for arm: just trace through the interpreter

--- arm/supportcodearm.py
+++ arm/supportcodearm.py
@@ -23,12 +23,8 @@ def check_file_missing(fn):
 def get_main(outarm):
     Machine = outarm.Machine
 
-    def get_printable_location(pc):
-        return hex(pc)
-
     driver = jit.JitDriver(
-        get_printable_location=get_printable_location,
-        greens=['pc'],
+        greens=[],
         reds=['machine'],
         name="arm",
         is_recursive=True)
@@ -40,20 +36,28 @@ def get_main(outarm):
             return step(machine, *args)
         # otherwise just do the main work here
         while 1:
-            driver.jit_merge_point(pc=machine._reg_z_PC, machine=machine)
+            driver.jit_merge_point(machine=machine)
             jit.promote(machine.g)
-            prev_pc = jit.promote(machine._reg_z_PC)
             step(machine, ())
             if machine.have_exception:
                 return ()
             cycle_count(machine, ())
-            newpc = machine._reg_z_PC
-            if prev_pc >= newpc: # backward jump
-                driver.can_enter_jit(pc=newpc, machine=machine)
     outarm.func_zstep_model = jitstep
 
     setinstr = outarm.func_z__SetThisInstr
     def jitsetinstr(machine, opcode):
+        # approach: promote opcode, but do it 4 bits at a time, to make sure we
+        # don't just get a linear search. start from the highest bits, because
+        # that's where the instruction-specific bits are
+        jit.jit_debug("arm-opcode", opcode)
+        jit.promote(opcode & 0xf0000000)
+        jit.promote(opcode & 0x0f000000)
+        jit.promote(opcode & 0x00f00000)
+        jit.promote(opcode & 0x000f0000)
+        jit.promote(opcode & 0x0000f000)
+        jit.promote(opcode & 0x00000f00)
+        jit.promote(opcode & 0x000000f0)
+        jit.promote(opcode & 0x0000000f)
         jit.promote(opcode)
         return setinstr(machine, opcode)
     outarm.func_z__SetThisInstr = jitsetinstr

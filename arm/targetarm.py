3a38cdf4aa91bf02347cf92b31c77fdc2a9addc2
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2023-09-02 16:05:21 +0200

    promote registers

--- arm/targetarm.py
+++ arm/targetarm.py
@@ -8,6 +8,36 @@ outarm = os.path.join(toplevel, "arm", "generated", "outarm.py")
 
 sys.setrecursionlimit(20000) # otherwise jitcode writing fails
 
+PROMOTED_REGISTERS = set("""
+zTCR_EL1
+z__empam_implemented
+zMAIR_EL3
+zMPAMIDR_EL1
+z__mpam_implemented
+zSCTLR_EL3
+zTCR_EL3
+zDBGEN
+z_EDSCR_0_28
+z_EDSCR_31_31
+zMDCCSR_EL0
+zMPAM3_EL3
+zOSLSR_EL1
+z__supported_pa_sizze
+zTSTATE
+zCNTCR
+zCNTHP_CTL_EL2
+zCNTHPS_CTL_EL2
+zCNTHV_CTL_EL2
+zCNTHVS_CTL_EL2
+zCNTP_CTL_EL0
+zCNTPS_CTL_EL1
+zMDCR_EL2
+zPSTATE
+zMDSCR_EL1
+zHCR_EL2
+zSCR_EL3
+""".split())
+
 def make_code(regen=True):
     from arm import supportcodearm
     outarm = _make_code(regen)
@@ -19,7 +49,7 @@ def _make_code(regen=True):
         with open(armir, "rb") as f:
             s = f.read()
         support_code = "from arm import supportcodearm as supportcode"
-        res = parse_and_make_code(s, support_code)
+        res = parse_and_make_code(s, support_code, PROMOTED_REGISTERS)
         with open(outarm, "w") as f:
             f.write(res)
         print "written file", outarm, "importing now"

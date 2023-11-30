3a97dd73d12abbcf4047a4315ba99e70b6ae2117
CF Bolz-Tereick <cfbolz@gmx.de>
2023-11-30 17:47:31 +0100

    slightly shorter tests

--- pydrofoil/test/test_ir.py
+++ pydrofoil/test/test_ir.py
@@ -8,6 +8,21 @@ class FakeCodeGen:
 
 fakecodegen = FakeCodeGen()
 
+def check_simplify(graph, expected):
+    res = simplify(graph, fakecodegen)
+    assert res
+    res = print_graph_construction(graph)
+    got = "\n".join(res)
+    if got != expected:
+        print "EXPECTED:"
+        print "_" * 60
+        print expected
+        print "GOT:"
+        print "_" * 60
+        print got
+    assert got == expected
+
+
 def make_bits_to_bool():
     zb = Argument('zb', SmallFixedBitVector(1))
     block0 = Block()
@@ -124,14 +139,11 @@ def test_cast():
     i2 = block0.emit(Cast, '$cast', [i1], SmallFixedBitVector(1), '`1 14:2-14:5', 'zz43')
     block0.next = Return(i2, '')
     graph = Graph('f', [zb], block0)
-    res = simplify(graph, fakecodegen)
-    assert res
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 zb = Argument('zb', SmallFixedBitVector(1))
 block0 = Block()
 block0.next = Return(zb, '')
-graph = Graph('f', [zb], block0)"""
+graph = Graph('f', [zb], block0)""")
 
 def test_eq_bits():
     za = Argument('za', SmallFixedBitVector(2))
@@ -141,14 +153,12 @@ def test_eq_bits():
     i3 = block0.emit(Operation, 'eq_bits', [i1, i2], Bool(), '`5 120:4-120:8', 'zz410')
     block0.next = Return(i3, None)
     graph = Graph("f", [za], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 za = Argument('za', SmallFixedBitVector(2))
 block0 = Block()
 i1 = block0.emit(Operation, '@eq_bits_bv_bv', [za, SmallBitVectorConstant(1, SmallFixedBitVector(2))], Bool(), '`5 120:4-120:8', 'zz410')
 block0.next = Return(i1, None)
-graph = Graph('f', [za], block0)"""
+graph = Graph('f', [za], block0)""")
 
 def test_neq_bits():
     za = Argument('za', SmallFixedBitVector(2))
@@ -158,15 +168,13 @@ def test_neq_bits():
     i3 = block0.emit(Operation, 'neq_bits', [i1, i2], Bool(), '`5 120:4-120:8', 'zz410')
     block0.next = Return(i3, None)
     graph = Graph("f", [za], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 za = Argument('za', SmallFixedBitVector(2))
 block0 = Block()
 i1 = block0.emit(Operation, '@eq_bits_bv_bv', [za, SmallBitVectorConstant(1, SmallFixedBitVector(2))], Bool(), '`5 120:4-120:8', 'zz410')
 i2 = block0.emit(Operation, '@not', [i1], Bool(), None, None)
 block0.next = Return(i2, None)
-graph = Graph('f', [za], block0)"""
+graph = Graph('f', [za], block0)""")
 
 def test_int_and_back():
     block0 = Block()
@@ -174,12 +182,10 @@ def test_int_and_back():
     i2 = block0.emit(Operation, 'int_to_int64', [i1], MachineInt(), '`5 295:30-295:32', 'zz40')
     block0.next = Return(i2)
     graph = Graph("f", [], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 block0 = Block()
 block0.next = Return(MachineIntConstant(15), None)
-graph = Graph('f', [], block0)"""
+graph = Graph('f', [], block0)""")
 
 def test_eq_int():
     zr = Argument('zr', MachineInt())
@@ -189,14 +195,12 @@ def test_eq_int():
     i3 = block0.emit(Operation, 'eq_int', [i2, i1], Bool(), '`83', 'zz4129')
     block0.next = Return(i3)
     graph = Graph("f", [zr], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 zr = Argument('zr', MachineInt())
 block0 = Block()
 i1 = block0.emit(Operation, '@eq', [zr, MachineIntConstant(0)], Bool(), '`83', 'zz4129')
 block0.next = Return(i1, None)
-graph = Graph('f', [zr], block0)"""
+graph = Graph('f', [zr], block0)""")
 
 def test_vector_subrange():
     zargz3 = Argument('zargz3', SmallFixedBitVector(64))
@@ -208,14 +212,12 @@ def test_vector_subrange():
     i5 = block0.emit(Cast, '$cast', [i4], SmallFixedBitVector(7), '`36 84:17-84:31', 'zz412111')
     block0.next = Return(i5)
     graph = Graph("f", [zargz3], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 zargz3 = Argument('zargz3', SmallFixedBitVector(64))
 block0 = Block()
 i1 = block0.emit(Operation, '@vector_subrange_fixed_bv_i_i', [zargz3, MachineIntConstant(6), MachineIntConstant(0)], SmallFixedBitVector(7), '`36 84:17-84:31', 'zz412140')
 block0.next = Return(i1, None)
-graph = Graph('f', [zargz3], block0)"""
+graph = Graph('f', [zargz3], block0)""")
 
 def test_vector_update_subrange():
     zv = Argument('zv', SmallFixedBitVector(64))
@@ -227,15 +229,13 @@ def test_vector_update_subrange():
     i6 = block0.emit(Cast, '$cast', [i5], SmallFixedBitVector(64), '`514', 'zz40')
     block0.next = Return(i6, None)
     graph = Graph('update', [zv, zx], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 zv = Argument('zv', SmallFixedBitVector(64))
 zx = Argument('zx', SmallFixedBitVector(1))
 block0 = Block()
 i2 = block0.emit(Operation, '@vector_update_subrange_fixed_bv_i_i_bv', [zv, MachineIntConstant(0), MachineIntConstant(0), zx], SmallFixedBitVector(64), '`513', 'zz47')
 block0.next = Return(i2, None)
-graph = Graph('update', [zv, zx], block0)"""
+graph = Graph('update', [zv, zx], block0)""")
 
 def test_vector_access():
     zx = Argument('zx', SmallFixedBitVector(8))
@@ -244,14 +244,12 @@ def test_vector_access():
     i2 = block0.emit(Operation, '@vector_access_o_i', [i1, MachineIntConstant(7)], Bit(), '`35 86:29-86:33', 'zz46')
     block0.next = Return(i2)
     graph = Graph('update', [zx], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 zx = Argument('zx', SmallFixedBitVector(8))
 block0 = Block()
 i1 = block0.emit(Operation, '@vector_access_bv_i', [zx, MachineIntConstant(7)], Bit(), '`35 86:29-86:33', 'zz46')
 block0.next = Return(i1, None)
-graph = Graph('update', [zx], block0)"""
+graph = Graph('update', [zx], block0)""")
 
 def test_and_not_bits():
     zx = Argument('zx', SmallFixedBitVector(64))
@@ -264,16 +262,14 @@ def test_and_not_bits():
     i8 = block0.emit(Cast, '$cast', [i4], SmallFixedBitVector(64), '`25 286:37-286:63', 'zz420')
     block0.next = Return(i8)
     graph = Graph('update', [zx, zy], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 zx = Argument('zx', SmallFixedBitVector(64))
 zy = Argument('zy', SmallFixedBitVector(64))
 block0 = Block()
 i2 = block0.emit(Operation, '@not_vec_bv', [zx, MachineIntConstant(64)], SmallFixedBitVector(64), '`25 286:51-286:62', None)
 i3 = block0.emit(Operation, '@and_vec_bv_bv', [zy, i2], SmallFixedBitVector(64), '`25 286:37-286:63', 'zz422')
 block0.next = Return(i3, None)
-graph = Graph('update', [zx, zy], block0)"""
+graph = Graph('update', [zx, zy], block0)""")
 
 def test_add_bits():
     zx = Argument('zx', SmallFixedBitVector(64))
@@ -285,15 +281,13 @@ def test_add_bits():
     i8 = block0.emit(Cast, '$cast', [i4], SmallFixedBitVector(64), '`25 286:37-286:63', 'zz420')
     block0.next = Return(i8)
     graph = Graph('update', [zx, zy], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 zx = Argument('zx', SmallFixedBitVector(64))
 zy = Argument('zy', SmallFixedBitVector(64))
 block0 = Block()
 i2 = block0.emit(Operation, '@add_bits_bv_bv', [zx, zy, MachineIntConstant(64)], SmallFixedBitVector(64), '`25 286:37-286:63', 'zz422')
 block0.next = Return(i2, None)
-graph = Graph('update', [zx, zy], block0)"""
+graph = Graph('update', [zx, zy], block0)""")
 
 def test_append():
     zcreg = Argument('zcreg', SmallFixedBitVector(3))
@@ -304,14 +298,12 @@ def test_append():
     i4 = block0.emit(Cast, '$cast', [i3], SmallFixedBitVector(5), '`5 100:30-100:41', 'return')
     block0.next = Return(i4, None)
     graph = Graph('zcreg2reg_idx', [zcreg], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 zcreg = Argument('zcreg', SmallFixedBitVector(3))
 block0 = Block()
 i1 = block0.emit(Operation, '@bitvector_concat_bv_bv', [SmallBitVectorConstant(1, SmallFixedBitVector(2)), MachineIntConstant(3), zcreg], SmallFixedBitVector(5), '`5 100:30-100:41', 'zz42')
 block0.next = Return(i1, None)
-graph = Graph('zcreg2reg_idx', [zcreg], block0)"""
+graph = Graph('zcreg2reg_idx', [zcreg], block0)""")
 
 def test_shiftr():
     for kind in "@shiftr_o_i", "@arith_shiftr_o_i":
@@ -324,16 +316,14 @@ def test_shiftr():
         i4 = block0.emit(Cast, '$cast', [i3], SmallFixedBitVector(64), '`26 426:31-426:71', 'zhtif_exit_code')
         block0.next = Return(i4, None)
         graph = Graph('zcreg2reg_idx', [arg], block0)
-        simplify(graph, fakecodegen)
-        res = print_graph_construction(graph)
-        assert "\n".join(res) == """\
+        check_simplify(graph, """\
 arg = Argument('arg', SmallFixedBitVector(64))
 block0 = Block()
 i1 = block0.emit(Operation, '@vector_subrange_fixed_bv_i_i', [arg, MachineIntConstant(47), MachineIntConstant(0)], SmallFixedBitVector(48), '`3484', 'zz44')
 i2 = block0.emit(Operation, '@zero_extend_bv_i_i', [i1, MachineIntConstant(48), MachineIntConstant(64)], SmallFixedBitVector(64), '`26 426:31-426:66', 'zz449')
 i3 = block0.emit(Operation, '%s_bv_i', [i2, MachineIntConstant(64), MachineIntConstant(1)], SmallFixedBitVector(64), '`26 426:31-426:71', 'zz445')
 block0.next = Return(i3, None)
-graph = Graph('zcreg2reg_idx', [arg], block0)""" % (kind[:-4], )
+graph = Graph('zcreg2reg_idx', [arg], block0)""" % (kind[:-4], ))
 
 def test_int_cmp():
     a1 = Argument('i', SmallFixedBitVector(52))
@@ -346,9 +336,7 @@ def test_int_cmp():
     i5 = block0.emit(Operation, 'lt', [i3, i4], Bool(), '`41 263:11-263:42', 'zz412')
     block0.next = Return(i5, None)
     graph = Graph('f', [a1, a2], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 i = Argument('i', SmallFixedBitVector(52))
 i = Argument('i', SmallFixedBitVector(52))
 block0 = Block()
@@ -356,7 +344,7 @@ i2 = block0.emit(Operation, '@unsigned_bv', [i, MachineIntConstant(52)], Machine
 i3 = block0.emit(Operation, '@unsigned_bv', [i, MachineIntConstant(52)], MachineInt(), '`41 263:29-263:42', 'zz427')
 i4 = block0.emit(Operation, '@lt', [i2, i3], Bool(), '`41 263:11-263:42', 'zz412')
 block0.next = Return(i4, None)
-graph = Graph('f', [i, i], block0)"""
+graph = Graph('f', [i, i], block0)""")
 
 
 def test_set_slice():
@@ -372,16 +360,14 @@ def test_set_slice():
     i8 = block0.emit(Cast, '$cast', [i7], SmallFixedBitVector(64), '`7 49:10-49:72', 'zz40')
     block0.next = Return(i8, None)
     graph = Graph('z__get_FPCR', [zval_name], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 zval_name = Argument('zval_name', SmallFixedBitVector(64))
 block0 = Block()
 i1 = block0.emit(Operation, '@vector_update_subrange_fixed_bv_i_i_bv', [zval_name, MachineIntConstant(7), MachineIntConstant(3), SmallBitVectorConstant(0, SmallFixedBitVector(5))], SmallFixedBitVector(64), '`7 47:10-47:38', 'zz421')
 i2 = block0.emit(Operation, '@vector_update_subrange_fixed_bv_i_i_bv', [i1, MachineIntConstant(14), MachineIntConstant(14), SmallBitVectorConstant(0, SmallFixedBitVector(1))], SmallFixedBitVector(64), '`7 48:10-48:35', 'zz414')
 i3 = block0.emit(Operation, '@vector_update_subrange_fixed_bv_i_i_bv', [i2, MachineIntConstant(63), MachineIntConstant(27), SmallBitVectorConstant(0, SmallFixedBitVector(37))], SmallFixedBitVector(64), '`7 49:10-49:72', 'zz47')
 block0.next = Return(i3, None)
-graph = Graph('z__get_FPCR', [zval_name], block0)"""
+graph = Graph('z__get_FPCR', [zval_name], block0)""")
 
 def test_zero_extend_unwrapped_res():
     value = Argument('value', GenericBitVector())
@@ -390,14 +376,12 @@ def test_zero_extend_unwrapped_res():
     i1 = block0.emit(Cast, '$cast', [i0], SmallFixedBitVector(64), '`7 3419:16-3419:38', 'zz42')
     block0.next = Return(i1, None)
     graph = Graph('f', [value], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 value = Argument('value', GenericBitVector())
 block0 = Block()
 i1 = block0.emit(Operation, '@zero_extend_o_i_unwrapped_res', [value, MachineIntConstant(64)], SmallFixedBitVector(64), '`5 234:29-234:46', 'return')
 block0.next = Return(i1, None)
-graph = Graph('f', [value], block0)"""
+graph = Graph('f', [value], block0)""")
 
 def test_sign_extend_unwrapped_res():
     value = Argument('value', GenericBitVector())
@@ -406,14 +390,12 @@ def test_sign_extend_unwrapped_res():
     i1 = block0.emit(Cast, '$cast', [i0], SmallFixedBitVector(64), '`7 3419:16-3419:38', 'zz42')
     block0.next = Return(i1, None)
     graph = Graph('f', [value], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 value = Argument('value', GenericBitVector())
 block0 = Block()
 i1 = block0.emit(Operation, '@sign_extend_o_i_unwrapped_res', [value, MachineIntConstant(64)], SmallFixedBitVector(64), '`5 234:29-234:46', 'return')
 block0.next = Return(i1, None)
-graph = Graph('f', [value], block0)"""
+graph = Graph('f', [value], block0)""")
 
 def test_vector_update_list():
     index = Argument('index', MachineInt())
@@ -425,16 +407,14 @@ def test_vector_update_list():
     block0.emit(GlobalWrite, 'z_R', [i9], FVec(31, SmallFixedBitVector(64)), None, None)
     block0.next = Return(None, None)
     graph = Graph('f', [index], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 index = Argument('index', MachineInt())
 block0 = Block()
 i1 = block0.emit(GlobalRead, 'z_R', [], FVec(31, SmallFixedBitVector(64)), None, None)
 i2 = block0.emit(Cast, '$cast', [i1], Vec(SmallFixedBitVector(64)), '`7 1087:12-1087:17', 'zz416')
 i3 = block0.emit(Operation, '@helper_vector_update_inplace_o_i_o', [i2, index, SmallBitVectorConstant(1, SmallFixedBitVector(2))], Unit(), '`7 1087:12-1087:17', 'zz418')
 block0.next = Return(None, None)
-graph = Graph('f', [index], block0)"""
+graph = Graph('f', [index], block0)""")
 
 def test_fill_fresh_vector():
     block0 = Block()
@@ -445,9 +425,7 @@ def test_fill_fresh_vector():
     i5 = block0.emit(Operation, 'zvalidDoubleRegs', [IntConstant(3), i4], Bool(), '`41 765:95-765:126', 'zz46820')
     block0.next = Return(i5, None)
     graph = Graph('f', [], block0)
-    simplify(graph, fakecodegen)
-    res = print_graph_construction(graph)
-    assert "\n".join(res) == """\
+    check_simplify(graph, """\
 block0 = Block()
 i0 = block0.emit(VectorInit, '$zinternal_vector_init', [MachineIntConstant(3)], Vec(MachineInt()), '`41 765:95-765:126', None)
 i1 = block0.emit(Operation, '@helper_vector_update_inplace_o_i_o', [i0, MachineIntConstant(0), MachineIntConstant(5)], Unit(), None, None)
@@ -455,5 +433,5 @@ i2 = block0.emit(Operation, '@helper_vector_update_inplace_o_i_o', [i0, MachineI
 i3 = block0.emit(Operation, '@helper_vector_update_inplace_o_i_o', [i0, MachineIntConstant(2), MachineIntConstant(9)], Unit(), None, None)
 i4 = block0.emit(Operation, 'zvalidDoubleRegs', [IntConstant(3), i0], Bool(), '`41 765:95-765:126', 'zz46820')
 block0.next = Return(i4, None)
-graph = Graph('f', [], block0)"""
+graph = Graph('f', [], block0)""")
 

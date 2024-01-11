51739849bd69bdc9ce641ce8db4cb6a500cf76b2
CF Bolz-Tereick <cfbolz@gmx.de>
2024-01-11 21:08:04 +0100

    add a way to run a lot more hypothesis examples

--- pydrofoil/test/test_supportcode.py
+++ pydrofoil/test/test_supportcode.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import math
 
@@ -10,6 +11,9 @@ from fractions import Fraction
 
 from rpython.rlib.rarithmetic import r_uint, intmask, r_ulonglong
 from rpython.rlib.rbigint import rbigint
+
+settings.register_profile("ci", max_examples=10000)
+settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
 
 def makelong_long_sequences(data, ndigits):
     """ From CPython:

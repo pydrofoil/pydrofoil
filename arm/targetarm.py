28ecba3c2f2157763edab184570dc627bb531e9d
CF Bolz-Tereick <cfbolz@gmx.de>
2024-01-16 15:21:10 +0100

    make sure that SetConfig is exposed

--- arm/targetarm.py
+++ arm/targetarm.py
@@ -63,7 +63,7 @@ def _make_code(regen=True):
     if regen:
         with open(armir, "rb") as f:
             s = f.read()
-        entrypoints = "zstep_model z__SetThisInstr zmain".split()
+        entrypoints = "zstep_model z__SetThisInstr zmain z__SetConfig".split()
         support_code = "from arm import supportcodearm as supportcode"
         res = parse_and_make_code(s, support_code, PROMOTED_REGISTERS,
                                   should_inline=should_inline,

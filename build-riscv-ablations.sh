023a2c8dc9fbd732bc77053994141fb625f3170a
CF Bolz-Tereick <cfbolz@gmx.de>
2025-02-04 11:27:22 +0100

    reduce number of ablations

--- build-riscv-ablations.sh
+++ build-riscv-ablations.sh
@@ -24,24 +24,3 @@ PYTHONPATH=. pypy_binary/bin/python pypy2/rpython/bin/rpython -O2 --output=pydro
 
 patch -p1 < ablate/no-small-integers.patch
 PYTHONPATH=. pypy_binary/bin/python pypy2/rpython/bin/rpython -O2 --output=pydrofoil-riscv-ablation-06-no-runtime-smallint riscv/targetriscv.py
-
-
-git reset --hard
-
-PYTHONPATH=. pypy_binary/bin/python pypy2/rpython/bin/rpython -O2 --output=pydrofoil-riscv-singleablation-02-no-jit-at-all riscv/targetriscv.py
-
-patch -p1 < ablate/bv-int-opt.patch
-PYTHONPATH=. pypy_binary/bin/python pypy2/rpython/bin/rpython -Ojit --output=pydrofoil-riscv-singleablation-03-no-static-int-bv-opts riscv/targetriscv.py
-
-git reset --hard
-patch -p1 < ablate/spec-inline.patch
-PYTHONPATH=. pypy_binary/bin/python pypy2/rpython/bin/rpython -Ojit --output=pydrofoil-riscv-singleablation-04-no-spec-no-inline riscv/targetriscv.py
-
-git reset --hard
-patch -p1 < ablate/spec-inline.patch
-patch -p1 < ablate/no-static-ir-opt.patch
-PYTHONPATH=. pypy_binary/bin/python pypy2/rpython/bin/rpython -Ojit --output=pydrofoil-riscv-singleablation-05-no-opt riscv/targetriscv.py
-
-git reset --hard
-patch -p1 < ablate/no-small-integers.patch
-PYTHONPATH=. pypy_binary/bin/python pypy2/rpython/bin/rpython -Ojit --output=pydrofoil-riscv-singleablation-06-no-runtime-smallint riscv/targetriscv.py

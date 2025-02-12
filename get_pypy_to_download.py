91d609f5c2fedd78c7eb1db246bd4e29e78e7fe1
CF Bolz-Tereick <cfbolz@gmx.de>
2025-02-12 13:31:24 +0100

    make pydrofoil switch to running nightly pypys

--- get_pypy_to_download.py
+++ get_pypy_to_download.py
@@ -4,11 +4,15 @@ Use the platform.uname and the versions.json from downloads.python.org to find
 the name of the latests pypy2.7 for this platform. Print it out on stdout
 """
 
+import sys
 import json
 from urllib import request, error
 import platform
 import tarfile
 import io
+
+
+want_nightly = "--nightly" in sys.argv
 
 response = request.urlopen('https://downloads.python.org/pypy/versions.json')
 if not (response.getcode(), 200):
@@ -23,17 +27,20 @@ def lookup_arch(machine):
     else:
         return machine
 
+found_versions = []
 for d in data:
-    if d['stable'] is True and "2.7" in d["python_version"]:
+    if d['stable'] == (not want_nightly) and "2.7" in d["python_version"]:
         for f in d["files"]:
             if f["arch"] == lookup_arch(uname.machine) and f["platform"] == uname.system.lower():
-                download = f["download_url"]
+                found_versions.append((d, f))
                 break
         else:
             raise RuntimeError(
                 f"No known stable PyPy2.7 build for {uname.machine}-{uname.system}"
             )
-        break
-else:
-    raise RuntimeError(f"No known stable PyPy2.7 build???")
+if want_nightly:
+    found_versions = [d for d in found_versions if d[0]['pypy_version'] == 'nightly']
+found_versions.sort(key=lambda x: x[0].get('date', None))
+
+download = found_versions[-1][1]["download_url"]
 request.urlretrieve(download, filename="pypy.tar.bz2")

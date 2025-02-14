5570a83c35ce73eb31ef08df9eec164f10a94aaa
CF Bolz-Tereick <cfbolz@gmx.de>
2025-02-14 14:09:26 +0100

    maybe fix logic

--- get_pypy_to_download.py
+++ get_pypy_to_download.py
@@ -29,18 +29,28 @@ def lookup_arch(machine):
 
 found_versions = []
 for d in data:
-    if d['stable'] == (not want_nightly) and "2.7" in d["python_version"]:
-        for f in d["files"]:
-            if f["arch"] == lookup_arch(uname.machine) and f["platform"] == uname.system.lower():
-                found_versions.append((d, f))
-                break
-        else:
-            raise RuntimeError(
-                f"No known stable PyPy2.7 build for {uname.machine}-{uname.system}"
-            )
-if want_nightly:
-    found_versions = [d for d in found_versions if d[0]['pypy_version'] == 'nightly']
-found_versions.sort(key=lambda x: x[0].get('date', None))
+    if not d["python_version"].startswith("2.7"):
+        continue
+    if want_nightly:
+        if d["pypy_version"] != "nightly":
+            continue
+    else:
+        if not d["stable"]:
+            continue
+    for f in d["files"]:
+        if f["arch"] == lookup_arch(uname.machine) and f["platform"] == uname.system.lower():
+            found_versions.append((d, f))
+            break
+if not found_versions:
+    if want_nightly:
+        kind = "nightly"
+    else:
+        kind = "stable"
+    raise RuntimeError(
+        f"No known {kind} PyPy2.7 build for {uname.machine}-{uname.system}"
+    )
+if len(found_versions) > 1:
+    found_versions.sort(key=lambda x: x[0].get('date', None))
 
 download = found_versions[-1][1]["download_url"]
 request.urlretrieve(download, filename="pypy.tar.bz2")

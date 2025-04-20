e21a9cd83d9fe3091492007d3c926a1617de2845
SimonRomanowski <lasagevo@gmail.com>
2025-04-20 09:59:47 +0200

    First implementation of global effects

--- pydrofoil/effectinfo.py
+++ pydrofoil/effectinfo.py
@@ -1,4 +1,5 @@
-from pydrofoil.ir import GlobalRead, Graph, GlobalWrite
+from collections import defaultdict
+from pydrofoil.ir import GlobalRead, Graph, GlobalWrite, Operation
 
 class EffectInfo(object):
 
@@ -10,7 +11,7 @@ class EffectInfo(object):
         # type: (str) -> EffectInfo
         return EffectInfo(
             self.register_reads,
-            self.register_writes | {register_name}
+            self.register_writes | {register_name},
         )
 
     def add_read(self, register_name):
@@ -19,6 +20,62 @@ class EffectInfo(object):
             self.register_reads | {register_name},
             self.register_writes,
         )
+
+    def extend(self, other):
+        # type: (EffectInfo) -> EffectInfo
+        """Create a new EffectInfo with all effects from 'other' added."""
+        return EffectInfo(
+            self.register_reads | other.register_reads,
+            self.register_writes | other.register_writes,
+        )
+
+    def __repr__(self):
+        return (
+            "EffectInfo(register_reads=%s, register_writes=%s)"
+            % (self.register_reads, self.register_writes)
+        )
+
+
+class _EffectComputationState(object):
+    def __init__(self):
+        self.effect_map = defaultdict(EffectInfo)  # type: dict[str, EffectInfo]
+        self.caller_map = defaultdict(set) # type: dict[str, set[str]]
+        self.todo_set = set()  # type: set[str]
+
+    def analyze_all(self, graph_map):
+        # type: (dict[str, Graph]) -> dict[str, EffectInfo]
+        self.todo_set.update(graph_map)
+        while self.todo_set:
+            graph_name = self.todo_set.pop()
+            graph = graph_map[graph_name]
+            effect_info = self._get_effect_info(graph)
+            # update caller map
+            for block in graph.iterblocks():
+                for op in block.operations:
+                    if isinstance(op, Operation) and op.name in graph_map:
+                        if op.name == graph_name:
+                            continue
+                        self.caller_map[op.name].add(graph_name)
+
+            old_effects = self.effect_map[graph_name]
+            self.effect_map[graph_name] = effect_info
+
+            # Update all callers
+            if old_effects != effect_info:
+                self.todo_set.update(self.caller_map[graph_name])
+
+        return self.effect_map
+
+    def _get_effect_info(self, graph):
+        # type: (Graph) -> EffectInfo
+        effect_info = local_effects(graph)
+        # Add effects from all called functions
+        for block in graph.iterblocks():
+            for op in block.operations:
+                if isinstance(op, Operation) and op.name in self.effect_map:
+                    effect_info = effect_info.extend(self.effect_map[op.name])
+        return effect_info
+
 
 def local_effects(graph):
     # type: (Graph) -> EffectInfo
@@ -30,3 +87,9 @@ def local_effects(graph):
             elif isinstance(op, GlobalRead):
                 result = result.add_read(op.name)
     return result
+
+
+def compute_all_effects(graph_map):
+    # type: (dict[str, Graph]) -> dict[str, EffectInfo]
+    state = _EffectComputationState()
+    return state.analyze_all(graph_map)

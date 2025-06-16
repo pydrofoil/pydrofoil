64eb1a14438e25dee5ec3ccefbf69065418da205
CF Bolz-Tereick <cfbolz@gmx.de>
2025-06-16 16:55:45 +0200

    what the interface could look like for ram

--- vcmlcore/pydrofoilcapi.h
+++ vcmlcore/pydrofoilcapi.h
@@ -14,3 +14,10 @@ CFFI_DLLEXPORT int pydrofoil_cpu_reset(void*);
 
 CFFI_DLLEXPORT int pydrofoil_cpu_set_verbosity(void*, int); // 0 = quiet, 1 = verbose
 CFFI_DLLEXPORT uint64_t pydrofoil_cpu_pc(void*);
+
+//CFFI_DLLEXPORT int pydrofoil_cpu_set_ram_write_callback(void*,
+//                                                       int (*)(uint64_t, int size, uint64_t, void*),
+//                                                       void*);
+//CFFI_DLLEXPORT int pydrofoil_cpu_set_ram_read_callback(void*,
+//                                                      int (*)(uint64_t, int size, uint64_t*, void*),
+//                                                      void*);

cd86453f83fec0c0bd94ed726d79a35edc4f9267
Christoph Jung <christoph.j27@googlemail.com>
2025-06-26 14:49:22 +0200

    test Z3BoolValue and, or

--- pydrofoil/test/test_z3backend_datatypes.py
+++ pydrofoil/test/test_z3backend_datatypes.py
@@ -86,7 +86,7 @@ def test_z3boolvalue_not_():
     assert res == z3.unsat
 
 def test_z3boolvalue_if():
-    """ Test if with BooleanConstant as condition """
+    """ Test if with Z3BoolValue as condition """
     a = z3.BitVec("a", 32)
     b = z3.BitVec("b", 32)
     w_val_a = Z3Value(a)
@@ -98,6 +98,61 @@ def test_z3boolvalue_if():
 
     solver = z3.Solver()
     res = solver.check(z3.Not(w_if.toz3() == z3.If(x, a, b)))
+    assert res == z3.unsat
+
+def test_z3boolvalue_or():
+    """ Test or on Z3BoolValue """
+    x = z3.Bool("x")
+    a = z3.Bool("a")
+    w_val = Z3BoolValue(x)
+    w_val_other = Z3Value(a)
+    
+    w_or_res = w_val._create_w_z3_or(w_val_other)
+
+    solver = z3.Solver()
+    res = solver.check(z3.Not(w_or_res.toz3() == z3.Or(x, a)))
+    assert res == z3.unsat
+
+    w_true = BooleanConstant(True)
+    w_or_res = w_val._create_w_z3_or(w_true)
+
+    assert w_or_res == w_true
+
+    w_false = BooleanConstant(False)
+    w_or_res = w_val._create_w_z3_or(w_false)
+
+    assert w_or_res == w_val
+
+def test_z3boolvalue_and():
+    """ Test and on Z3BoolValue """
+    x = z3.Bool("x")
+    w_val = Z3BoolValue(x)
+    vals_w = []
+    
+    w_and_res = w_val._create_w_z3_and(*vals_w)
+
+    assert w_and_res is w_val
+
+
+    vals_w = [BooleanConstant(True)]
+    w_and_res = w_val._create_w_z3_and(*vals_w)
+
+    assert w_and_res is w_val
+
+
+    vals_w = [BooleanConstant(True), BooleanConstant(True),  BooleanConstant(False)]
+    w_and_res = w_val._create_w_z3_and(*vals_w)
+
+    assert isinstance(w_and_res, BooleanConstant)
+    assert w_and_res.value == False
+
+    a = z3.Bool("a")
+    b = z3.Bool("b")
+    vals_w = [BooleanConstant(True), BooleanConstant(True),  Z3BoolValue(a), Z3BoolNotValue(b)]
+    w_and_res = w_val._create_w_z3_and(*vals_w)
+    
+    solver = z3.Solver()
+    res = solver.check(z3.Not(w_and_res.toz3() == z3.And(x, a, z3.Not(b))))
     assert res == z3.unsat
 
 
@@ -116,7 +171,7 @@ def test_z3boolnotvalue_not_():
     assert res == z3.unsat
 
 def test_z3boolnotvalue_if():
-    """ Test if with BooleanConstant as condition """
+    """ Test if with Z3BoolNotValue as condition """
     a = z3.BitVec("a", 32)
     b = z3.BitVec("b", 32)
     w_val_a = Z3Value(a)
@@ -131,3 +186,57 @@ def test_z3boolnotvalue_if():
     assert res == z3.unsat
 
 
+def test_z3boolnotvalue_or():
+    """ Test or on Z3BoolNotValue """
+    x = z3.Bool("x")
+    a = z3.Bool("a")
+    w_val = Z3BoolNotValue(x)
+    w_val_other = Z3Value(a)
+    
+    w_or_res = w_val._create_w_z3_or(w_val_other)
+
+    solver = z3.Solver()
+    res = solver.check(z3.Not(w_or_res.toz3() == z3.Or(z3.Not(x), a)))
+    assert res == z3.unsat
+
+    w_true = BooleanConstant(True)
+    w_or_res = w_val._create_w_z3_or(w_true)
+
+    assert w_or_res == w_true
+
+    w_false = BooleanConstant(False)
+    w_or_res = w_val._create_w_z3_or(w_false)
+
+    assert w_or_res == w_val
+
+def test_z3boolnotvalue_and():
+    """ Test and on Z3BoolNotValue """
+    x = z3.Bool("x")
+    w_val = Z3BoolNotValue(x)
+    vals_w = []
+    
+    w_and_res = w_val._create_w_z3_and(*vals_w)
+
+    assert w_and_res is w_val
+
+
+    vals_w = [BooleanConstant(True)]
+    w_and_res = w_val._create_w_z3_and(*vals_w)
+
+    assert w_and_res is w_val
+
+
+    vals_w = [BooleanConstant(True), BooleanConstant(True),  BooleanConstant(False)]
+    w_and_res = w_val._create_w_z3_and(*vals_w)
+
+    assert isinstance(w_and_res, BooleanConstant)
+    assert w_and_res.value == False
+
+    a = z3.Bool("a")
+    b = z3.Bool("b")
+    vals_w = [BooleanConstant(True), BooleanConstant(True),  Z3BoolValue(a), Z3BoolNotValue(b)]
+    w_and_res = w_val._create_w_z3_and(*vals_w)
+    
+    solver = z3.Solver()
+    res = solver.check(z3.Not(w_and_res.toz3() == z3.And(z3.Not(x), a, z3.Not(b))))
+    assert res == z3.unsat

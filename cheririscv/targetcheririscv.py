b2a05614494060b8dfa832b60a0ba829944ae5f9
CF Bolz-Tereick <cfbolz@gmx.de>
2026-02-24 16:28:16 +0100

    support for cheri-riscv

---
+++ cheririscv/targetcheririscv.py
@@ -0,0 +1,101 @@
+import sys
+
+sys.setrecursionlimit(2 ** 31 - 1)
+
+from pydrofoil.makecode import *
+
+import os
+
+thisdir = os.path.dirname(__file__)
+
+riscvirs = [
+    os.path.join(thisdir, ""),
+    os.path.join(thisdir, "cheririscv_model_rv64.ir"),
+]
+outriscvpys = [
+    os.path.join(thisdir, "generated/outriscv32.py"),
+    os.path.join(thisdir, "generated/outriscv.py"),
+]
+
+
+def should_inline(name):
+    if "zextensionEnabled" in name:
+        return True
+    if "zmatch_TLB_Entry" in name:
+        return True
+    if "zsail_mem_read" in name:
+        return True
+    if "zsail_mem_write" in name:
+        return True
+    if "request_is_exclusive" in name:
+        return True
+
+
+def _make_code(rv64=True):
+    assert rv64
+    print "making python code"
+    with open(riscvirs[rv64], "rb") as f:
+        s = f.read()
+    support_code = "from cheririscv import supportcodecheririscv as supportcode"
+    entrypoints = "ztick_clock ztick_platform zinit_model zstep zext_decode zencdec_forwards zencdec_compressed_backwards".split()
+    res = parse_and_make_code(
+        s,
+        support_code,
+        {
+            "zPC",
+            "znextPC",
+            "zMisa_chunk_0",
+            "zcur_privilege",
+            "zMstatus_chunk_0",
+        },
+        entrypoints=entrypoints,
+        should_inline=should_inline,
+    )
+    with open(outriscvpys[rv64], "w") as f:
+        f.write(res)
+    if rv64:
+        from cheririscv.generated import outriscv
+    else:
+        from cheririscv.generated import outriscv32 as outriscv
+    return outriscv
+
+
+def make_code(rv64=True):
+    from cheririscv import supportcodecheririscv
+
+    outriscv = _make_code(rv64)
+    return supportcodecheririscv.get_main(outriscv, rv64)
+
+
+def make_code_combined():
+    from cheririscv import supportcodecheririscv
+
+    mod64 = _make_code(True)
+    machinecls64 = supportcodecheririscv.get_main(mod64, True)._machinecls
+    mod32 = _make_code(False)
+    machinecls32 = supportcodecheririscv.get_main(mod32, False)._machinecls
+
+    def bound_main(argv):
+        return supportcodecheririscv.main(argv, machinecls64, machinecls32)
+
+    return bound_main
+
+
+def target(*args):
+    import py
+
+    main = make_code_combined()
+    print "translating to C!"
+    return main
+
+
+if __name__ == "__main__":
+    import sys
+
+    try:
+        target()(sys.argv)
+    except:
+        import pdb
+
+        pdb.xpm()
+        raise
